e1cddb67811a083a19607486abd347288ffd9e9f
Igor Pavlenkov <pavlenkov.igor@mail.ru>
2020-01-31 18:29:16 +0300

    31.01.2020

---
+++ 2.JavaCore/src/com/javarush/task/mywork/ClassesAndObjects.java
@@ -0,0 +1,45 @@
+package com.javarush.task.mywork;
+
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "BOB";
+        person1.age = 22;
+        person1.speak();
+        person1.sayHello();
+        int year1 = person1.calculateYearsToRetirement();
+
+
+        Person person2 = new Person();
+        person2.name = "Igor";
+        person2.age = 26;
+        person2.speak();
+        int year2 = person2.calculateYearsToRetirement();
+
+        System.out.println(year1 +" "+year2);
+
+
+    }
+}
+ class Person{
+    String name;
+    int age;
+
+    int calculateYearsToRetirement()
+    {
+        int years = 65 - age;
+       // System.out.println("When will to retire? Through: " + years);
+        return years;
+    }
+
+    void speak() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello, my name is " + name + ". I am have is " + age + " year old");
+        }
+    }
+        void sayHello(){
+            System.out.println("Test");
+        }
+    }
+
+
